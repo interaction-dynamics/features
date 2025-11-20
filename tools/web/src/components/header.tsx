b5f63eee7cee783bb9c1bb33b3aa815bd37aa5ef
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-11-20 07:44:32 -0500

    fix: sticky header

--- tools/web/src/components/header.tsx
+++ tools/web/src/components/header.tsx
@@ -9,7 +9,7 @@ type HeaderProps = {
 
 export function Header({ children }: HeaderProps) {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+    <header className="sticky top-0 bg-background z-50 flex h-16 shrink-0 items-center gap-2 border-b">
       <div className="flex items-center gap-2 px-3">
         <SidebarTrigger />
         <Separator orientation="vertical" className="mr-2 h-4" />
