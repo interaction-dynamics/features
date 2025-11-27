292604b6ea592c71047e472b951ec13f6b38c797
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-11-27 10:28:42 -0500

    test: exclude e2e test from unit tests

--- tools/web/vite.config.ts
+++ tools/web/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,5 +17,8 @@ export default defineConfig({
       // it seems this property doesn't work yet
       external: ['**/features.json'],
     },
+  },
+  test: {
+    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
 })
