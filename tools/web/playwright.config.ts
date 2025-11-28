e7193930449a3ffb049667ea13929cac39a38493
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-11-28 16:43:08 -0500

    test: use bun for playwright

--- tools/web/playwright.config.ts
+++ tools/web/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'pnpm dev',
+    command: 'bun dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
